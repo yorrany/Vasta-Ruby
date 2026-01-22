56c62bfaa65ca6c476ff21d5eecfa3d150cc4b42
Yorrany Martins Braga <yorranymb@gmail.com>
2026-01-22 18:53:51 -0400

    chore: cleanup legacy backend artifacts
    - Remove Docker configs
    
    - Remove Go backend source code
    
    - Project is now pure Next.js Serverless

--- config/initializers/stripe.rb
+++
@@ -1,4 +0,0 @@
-if ENV["STRIPE_SECRET_KEY"]
-  Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
-end
-
--- db/migrate/20260115001000_add_billing_to_tenants.rb
+++
@@ -1,9 +0,0 @@
-class AddBillingToTenants < ActiveRecord::Migration[7.1]
-  def change
-    add_column :tenants, :current_plan_code, :string, null: false, default: "start"
-    add_column :tenants, :billing_status, :string, null: false, default: "active"
-    add_column :tenants, :blocked_at, :datetime
-    add_column :tenants, :stripe_customer_id, :string
-  end
-end
-
