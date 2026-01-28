988642cc7e4dba91054db7228f05f8e11cd0dc83
Your Name <you@example.com>
2026-01-28 19:48:25 -0400

    chore: Remove multiple SHA256 hashes from the Content-Security-Policy script-src directive.

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self';",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://vercel.live https://*.vercel.live https://js.stripe.com https://m.stripe.network 'sha256-7PZaH7TzFg4JdT5xJguN7Och6VcMcP1LW4N3fQ936Fs=' 'sha256-MqH8JJslY2fF2bGYY1rZlpCNrRCnWKRzrrDefixUJTI=' 'sha256-ZswfTY7H35rbv8WC7NXBoiC7WNu86vSzCDChNWwZZDM=' 'sha256-e357n1PxCJ8d03/QCSKaHFmHF1JADyvSHdSfshxM494=' 'sha256-5DA+a07wxWmEka9IdoWjSPVHb17Cp5284/lJzfbl8KA=' 'sha256-/5Guo2nzv5n/w6ukZpOBZOtTJBJPSkJ6mhHpnBgm3Ls=' 'sha256-n46vPwSWuMC0W703pBofImv82Z26xo4LXymv0E9caPk=' 'sha256-OBTN3RiyCV4Bq7dFqZ5a2pAXjnCcCYeTJMO2I/LYKeo=' 'sha256-bGD16sEDcMz7YsLwirOdrIpO8GImPnyZ9pgPLd9BoOw=' 'sha256-kxaiXqyV/VekRAoS36EaM/jOWmpOtYfscVhWvVKeD70=' 'sha256-+wZjPvQnsa7f7tfyy+vIuVld0j8hGaUSFFAD8DiV8NU=' 'sha256-eNLke58MF3le0iRyyTxTjFwsUKW2BlrXs418x2LniLk=' 'sha256-UcIHlBHEQUDFaIf6Rd8Q4+HtUNdA1x312dQD92iwPG0=' 'sha256-ezRyN6OWfn0VPI7+2j2xrOmpM9wutmz0mTXdhEpTrAc=' 'sha256-ZNJGyl2ySKLn8ciUhVy98rWTAJU+WoOwNf2euWlVCbQ=' https://*.vercel-scripts.com https://va.vercel-scripts.com;",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://vercel.live https://*.vercel.live https://js.stripe.com https://m.stripe.network https://*.vercel-scripts.com https://va.vercel-scripts.com;",
               "child-src https://challenges.cloudflare.com https://vercel.live https://*.vercel.live https://js.stripe.com https://hooks.stripe.com;",
               "style-src 'self' 'unsafe-inline';",
               "img-src 'self' blob: data: https:;",
